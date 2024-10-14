897167b74f0dc80817c1af2eb566509b6bf6926a
ysrkw <15919660+ysrkw@users.noreply.github.com>
2024-10-15 02:45:18 +0900

    add perfectionist

--- apps/client-api/eslint.config.mjs
+++ apps/client-api/eslint.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 
 import eslint from '@eslint/js'
+import perfectionist from 'eslint-plugin-perfectionist'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  perfectionist.configs['recommended-alphabetical'],
   {
     ignores: ['dist'],
   },
