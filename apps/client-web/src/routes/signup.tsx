131949a9f627c059c8f51f0b18c248ad68dd79ff
ysrkw <15919660+ysrkw@users.noreply.github.com>
2024-10-28 03:45:24 +0900

    fix code

--- apps/client-web/src/routes/signup.tsx
+++ apps/client-web/src/routes/signup.tsx
@@ -1,46 +1,43 @@
-import { Suspense } from 'react'
 import { ActionFunctionArgs, Form, redirect } from 'react-router-dom'
 
 export function Signup() {
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Form action="/api/signup" method="POST">
-        <div>
-          <label htmlFor="email">メールアドレス</label>
-          <input id="email" name="email" required type="email" />
-        </div>
-        <div>
-          <label htmlFor="password">パスワード</label>
-          <input
-            id="password"
-            maxLength={80}
-            minLength={8}
-            name="password"
-            required
-            type="password"
-          />
-        </div>
-        <div>
-          <label htmlFor="terms-of-service">利用規約の同意</label>
-          <input
-            id="terms-of-service"
-            name="terms-of-service"
-            required
-            type="checkbox"
-          />
-        </div>
-        <div>
-          <label htmlFor="privacy-policy">プライバシーポリシーの同意</label>
-          <input
-            id="privacy-policy"
-            name="privacy-policy"
-            required
-            type="checkbox"
-          />
-        </div>
-        <button type="submit">アカウント作成</button>
-      </Form>
-    </Suspense>
+    <Form action="/api/signup" method="POST">
+      <div>
+        <label htmlFor="email">メールアドレス</label>
+        <input id="email" name="email" required type="email" />
+      </div>
+      <div>
+        <label htmlFor="password">パスワード</label>
+        <input
+          id="password"
+          maxLength={80}
+          minLength={8}
+          name="password"
+          required
+          type="password"
+        />
+      </div>
+      <div>
+        <label htmlFor="terms-of-service">利用規約の同意</label>
+        <input
+          id="terms-of-service"
+          name="terms-of-service"
+          required
+          type="checkbox"
+        />
+      </div>
+      <div>
+        <label htmlFor="privacy-policy">プライバシーポリシーの同意</label>
+        <input
+          id="privacy-policy"
+          name="privacy-policy"
+          required
+          type="checkbox"
+        />
+      </div>
+      <button type="submit">アカウント作成</button>
+    </Form>
   )
 }
 
